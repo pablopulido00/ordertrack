d509fdfd4fa86a605f4c6cd71f62ce906c87721c
pablopulido00 <pablopulidosolis@gmail.com>
2026-02-17 19:28:37 +0100

    Initial project setup: Spring Boot + Docker + Flyway V1

---
+++ src/main/java/com/ordertrack/orderdertrack/OrderdertrackApplication.java
@@ -0,0 +1,13 @@
+package com.ordertrack.orderdertrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderdertrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderdertrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ordertrack/orderdertrack/OrderdertrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ordertrack.orderdertrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderdertrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
